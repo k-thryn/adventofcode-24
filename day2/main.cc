3fed15b980a845efc21f3446d4c1c657cd45186a
Kathryn Stavish <kmstavish26@gmail.com>
2024-12-15 21:21:18 +0000

    oopsie

--- day2/main.cc
+++ day2/main.cc
@@ -1,9 +1,21 @@
 // day 2
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    // Read input file.
+    ifstream input("input.txt");
+      string line;
+
+    if (input.is_open())
+    {
+        while (input)
+        {
+              getline(input, line);
+        }
+    }
     return 0;
 }
