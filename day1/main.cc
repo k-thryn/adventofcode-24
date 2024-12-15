ea65d935b0aa6f82d5081a1d94aef980a55a39cb
Kathryn Stavish <kmstavish26@gmail.com>
2024-12-15 16:40:13 +0000

    fix formatting

--- day1/main.cc
+++ day1/main.cc
@@ -1,4 +1,4 @@
-// day 1 
+// day 1
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void calculate_distances(vector<int> list1, vector<int> list2) 
+void calculate_distances(vector<int> list1, vector<int> list2)
 {
     int total_distance = 0;
     for (int i = 0; i < list1.size(); i++)
@@ -22,9 +22,9 @@ void calculate_similarity_score(vector<int> list1, vector<int> list2)
 {
     int similarity_score = 0;
 
-    // index for iterating through list2. 
+    // index for iterating through list2.
     int j = 0;
-    for (int i = 0; i < list1.size(); i++) 
+    for (int i = 0; i < list1.size(); i++)
     {
         if (list1[i] < list2[j])
         {
@@ -32,10 +32,10 @@ void calculate_similarity_score(vector<int> list1, vector<int> list2)
         }
         while (list1[i] > list2[j])
         {
-            // Increment j until we find it. 
+            // Increment j until we find it.
             j++;
         }
-        // Count how many times the element in list1 appears in list2. 
+        // Count how many times the element in list1 appears in list2.
         int num_appearances = 0;
         while (list1[i] == list2[j])
         {
@@ -47,42 +47,42 @@ void calculate_similarity_score(vector<int> list1, vector<int> list2)
     cout << "Solution: " << similarity_score << "\n";
 }
 
-int main() 
+int main()
 {
-// Read input file.
-ifstream input ("input.txt"); 
-vector<int> list1;
-vector<int> list2;
-string line;
+    // Read input file.
+    ifstream input("input.txt");
+    vector<int> list1;
+    vector<int> list2;
+    string line;
 
-if (input.is_open()) 
-{
-    while (input) 
+    if (input.is_open())
     {
-        getline(input, line);
-        if (line.empty())
+        while (input)
         {
-            break;
+            getline(input, line);
+            if (line.empty())
+            {
+                break;
+            }
+            // Split into items from list 1 and list 2.
+            stringstream ss(line);
+            string loc;
+            // Read item from list 1.
+            ss >> loc;
+            list1.push_back(stoi(loc));
+            // Read item from list 2.
+            ss >> loc;
+            list2.push_back(stoi(loc));
         }
-        // Split into items from list 1 and list 2.
-        stringstream ss(line);
-        string loc;
-        // Read item from list 1.
-        ss >> loc;
-        list1.push_back(stoi(loc));
-        // Read item from list 2.
-        ss >> loc;
-        list2.push_back(stoi(loc));
     }
-}
-input.close();
+    input.close();
 
-// Sort each list of locations in ascending order. 
-sort(list1.begin(), list1.end());
-sort(list2.begin(), list2.end());
+    // Sort each list of locations in ascending order.
+    sort(list1.begin(), list1.end());
+    sort(list2.begin(), list2.end());
 
-calculate_distances(list1, list2);
-calculate_similarity_score(list1, list2);
+    calculate_distances(list1, list2);
+    calculate_similarity_score(list1, list2);
 
-return 0;
+    return 0;
 }
